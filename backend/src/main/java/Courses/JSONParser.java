f9433e8c1ae0b7452936cbe5afec26fbe993d8ff
jhmlee <jhmlee07@gmail.com>
2024-04-20 14:49:40 -0400

    feat: added small mock and parser

---
+++ backend/src/main/java/Courses/JSONParser.java
@@ -0,0 +1,64 @@
+package Courses;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.Moshi;
+
+public class JSONParser {
+
+  CourseObject parsedJSON;
+
+  /**
+   * Parses JSON data from a JsonReader and converts it to the specified target type.
+   *
+   * @param source The JsonReader containing the JSON data.
+   * @param targetType The Class representing the target data type to convert the JSON to.
+   * @param <T> The generic type of the target data.
+   * @return An instance of the target data type parsed from the JSON.
+   * @throws IOException if there's an error reading or parsing the JSON data.
+   */
+  public static <T> T fromJsonGeneral(String source, Class<T> targetType) throws IOException {
+    Moshi moshi = new Moshi.Builder().build();
+    JsonAdapter<T> adapter = moshi.adapter(targetType);
+    //    source.setLenient(true);
+
+    return adapter.fromJson(source);
+  }
+  public CourseObject getParsedJSON() {
+    return this.parsedJSON;
+  }
+
+  public void createCourses() {
+    String filePath = "data/mockCourses.json";
+    try {
+      // ***************** READING THE FILE *****************
+      FileReader jsonReader = new FileReader(filePath);
+      BufferedReader br = new BufferedReader(jsonReader);
+      String fileString = "";
+      String line = br.readLine();
+      while (line != null) {
+        fileString = fileString + line;
+        line = br.readLine();
+      }
+      jsonReader.close();
+
+      // ****************** CREATING THE ADAPTER **********
+      this.parsedJSON = this.fromJsonGeneral(fileString, CourseObject.class);
+
+    } catch (IOException e) {
+      System.out.println(e.getMessage());
+    }
+  }
+
+  public static void main(String[] args) throws FileNotFoundException {
+    JSONParser myparser = new JSONParser();
+    myparser.createCourses();
+
+    System.out.println("done");
+
+  }
+
+
+}
