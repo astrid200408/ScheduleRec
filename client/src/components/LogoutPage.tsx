2b703472d84c71caabfc546fd1bcc1dbbeb66400
simd22 <113197504+simd22@users.noreply.github.com>
2024-05-01 15:31:15 -0400

    feat: continuing main page

--- client/src/components/LogoutPage.tsx
+++ client/src/components/LogoutPage.tsx
@@ -6,42 +6,40 @@ const LogoutPage = () => {
     <div className="logout-page">
       <div className="input-fields">
         <div className="include-div">
-          <button className="include-button">include</button>
+          <button className="include-button">Include</button>
           <input
             className="include-input"
             type="text"
-            // value={inputValue}
-            // onChange={handleChange}
-            placeholder="Type something..."
+            placeholder="Type like this: csci1800,csci111,csci200"
           />
         </div>
 
         <div className="department-div">
-          <button className="department-button">dept.</button>
+          <button className="department-button">Dept.</button>
           <input
             className="department-input"
             type="text"
-            // value={inputValue}
-            // onChange={handleChange}
-            placeholder="Type something..."
+            placeholder="Type something like this: CSCI,ENGN,MATH"
           />
         </div>
       </div>
 
       <div className="button-div">
         <div className="class-buttons">
-          <button className="class-1"></button>
-          <button className="class-2"></button>
-          <button className="class-3"></button>
-          <button className="class-4"></button>
-          <button className="class-5"></button>
+          <button className="class-1">1</button>
+          <button className="class-2">2</button>
+          <button className="class-3">3</button>
+          <button className="class-4">4</button>
+          <button className="class-5">5</button>
         </div>
 
         <div className="hours-buttons">
-          <button className="less20"></button>
-          <button className="btwn2030"></button>
-          <button className="plus30"></button>
-          <button className="any"></button>
+          <button className="less20">&lt; 20</button>
+          <button className="btwn2030">20-30</button>
+          <button className="plus30">30 &#60;</button>
+          <button className="any">
+            any
+          </button>
         </div>
       </div>
 
@@ -52,11 +50,12 @@ const LogoutPage = () => {
           <div className="wedbox"> </div>
           <div className="thursbox"> </div>
           <div className="fribox"> </div>
-
         </div>
       </div>
     </div>
   );
 }
 
+  const changeColor = async (Button: button) => {
+  };
 export default LogoutPage
