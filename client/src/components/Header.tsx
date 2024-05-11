4f719c07c22ec383e47c05f2eeaa72d3cdd7658b
simd22 <113197504+simd22@users.noreply.github.com>
2024-05-11 10:33:57 -0400

    feat: updated header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,14 +1,22 @@
-import React from 'react'
-import "../styles/header.css"
-
+import React from "react";
+import "../styles/header.css";
+const imageLink =
+  "https://1000logos.net/wp-content/uploads/2022/05/Brown-University-Logo.png";
 const Header = () => {
   return (
     <header className="header">
-      <div className="profile">Profile</div>
+      {/* <div className="profile">Profile</div> */}
+      <img
+        className="brown-image"
+        src={imageLink}
+        alt="Brown University logo"
+        aria-label="Brown University logo"
+        aria-description="Brown University logo and Brown"
+      />
       <div className="title">The Critical Schedule Maker</div>
-      <div className="about">About</div>
+      <div className="about"> </div>
     </header>
   );
-}
+};
 
-export default Header
+export default Header;
